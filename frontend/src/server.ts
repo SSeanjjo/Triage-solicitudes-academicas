dfd2c03ab5d62cac7990ac92cd93e69b5bf491f7
SSeanjjo <142127854+SSeanjjo@users.noreply.github.com>
2026-05-29 02:10:02 -0500

    Fix: cambio allowed hosts

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -8,14 +8,16 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { join } from 'node:path';
 
+process.env['ANGULAR_SSR_ALLOWED_HOSTS'] = '*';
+
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
-// Redirige todas las peticiones /api al backend (configurable por variable de entorno)
 const apiTarget = process.env['API_URL'] || 'http://localhost:8080';
 console.log('API_URL configurada:', apiTarget);
+
 app.use('/api', createProxyMiddleware({
   target: apiTarget,
   changeOrigin: true,
@@ -38,34 +40,23 @@ app.use(
   }),
 );
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
 app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res as any) : next(),
     )
     .catch(next);
 });
 
-/**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
     }
-
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
-/**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
- */
 export const reqHandler = createNodeRequestHandler(app);
